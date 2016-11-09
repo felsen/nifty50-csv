5c69763401a9bb9801953fa9e9774240046ce538
FeLiX StEpHeN <felsen@localhost.localdomain>
2016-11-09 16:31:44 +0530

    Removed: commanted lines

--- nifty_50_csv.py
+++ nifty_50_csv.py
@@ -17,23 +17,14 @@ table_data = [j for i in table for j in i.find_all("tr")]
 table_header = [str(j.text.strip()) for d in table_data[0:1]
                 for j in d.find_all("th")]
 table_value = table_data[1:]
-# final_dict = {}
 column_header = ["Company_Name", "Open", "High", "Low", "LTP", "Change", "LTP1", "Percentage_Change", "Shares_Traded", "Trades", "Date_Updated"]
 dataset = []
 for d in table_value:
     cols = d.find_all("td")
     cols = [str(element.text.strip()) for element in cols]
     dataset.append(tuple(cols))
-#    final_dict[cols[0]] = cols
 
 data_frame = pd.DataFrame(data=dataset, columns=column_header)
 data_frame['Date_Updated'] = pd.to_datetime(data_frame['Date_Updated'], format="%m/%d/%Y")
 now = datetime.datetime.now()
 csv_frame = data_frame.to_csv("today_{0}.csv".format(now), index=False)
-json_converter = data_frame.to_json(path_or_buf=None, orient="records", date_format="epoch", double_precision=10, force_ascii=True, date_unit="ms", default_handler=None)
-
-#data_dict = {}
-#data_dict['name'] = "Share Market"
-#data_dict['children'] = eval(json_converter)
-#print data_dict
-
