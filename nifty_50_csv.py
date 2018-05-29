da0cb30cc5e6cbd507e2947f21eaa8c1593f8eca
Felix Stephen <felix_s@dell.com>
2018-05-29 11:37:54 +0545

    Modified: line based code in functional based code

--- nifty_50_csv.py
+++ nifty_50_csv.py
@@ -5,23 +5,37 @@ import pandas as pd
 import datetime
 
 
-base_url = "http://www.chittorgarh.com/report/nse_nifty_index_50_stocks_live_list/7/"
-user_agent = UserAgent()
-headers = { 'User-Agent': str(user_agent.random) }
-req = urllib2.Request(base_url, None, headers)
-read_url = urllib2.urlopen(req)
-response = read_url.read()
-soup = BeautifulSoup(response, "lxml")
-table = soup.find_all("table", attrs={'class': 'table'})
-table_data = [j for i in table for j in i.find_all("tr")]
-table_value = table_data[1:]
-column_header = ["Company_Name", "Open", "High", "Low", "LTP", "Change", "LTP1", "Percentage_Change", "Shares_Traded", "Trades", "Date_Updated"]
-dataset = []
-for d in table_value:
-    cols = d.find_all("td")
-    cols = [str(element.text.strip()) for element in cols]
-    dataset.append(tuple(cols))
-data_frame = pd.DataFrame(data=dataset, columns=column_header)
-data_frame['Date_Updated'] = pd.to_datetime(data_frame['Date_Updated'], format="%m/%d/%Y")
-now = datetime.datetime.now()
-csv_frame = data_frame.to_csv("today_{0}.csv".format(now), index=False)
+def main():
+    """
+    Download live nifty fifty details in CSV formated file.
+    """
+    base_url = "http://www.chittorgarh.com/report/nse_nifty_index_50_stocks_live_list/7/"
+    user_agent = UserAgent()
+    headers = { 'User-Agent': str(user_agent.random) }
+    req = urllib2.Request(base_url, None, headers)
+    read_url = urllib2.urlopen(req)
+    response = read_url.read()
+    soup = BeautifulSoup(response, "lxml")
+    table = soup.find_all("table", attrs={'class': 'table'})
+    table_data = [j for i in table for j in i.find_all("tr")]
+    table_value = table_data[1:]
+    column_header = ["Company_Name", "Open",
+                     "High", "Low", "LTP",
+                     "Change", "LTP1",
+                     "Percentage_Change",
+                     "Shares_Traded", "Trades",
+                     "Date_Updated"]
+    dataset = []
+    for d in table_value:
+        cols = d.find_all("td")
+        cols = [str(element.text.strip()) for element in cols]
+        dataset.append(tuple(cols))
+    data_frame = pd.DataFrame(data=dataset, columns=column_header)
+    data_frame['Date_Updated'] = pd.to_datetime(data_frame['Date_Updated'], format="%m/%d/%Y")
+    now = datetime.datetime.now()
+    csv_frame = data_frame.to_csv("today_{0}.csv".format(now), index=False)
+    return None
+
+
+if __name__ == "__main__":
+    main()
